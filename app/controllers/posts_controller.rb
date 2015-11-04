b48c03288a660653a7fef7b7806c62610537d3f0
AlvinChiu <alvinhsian@gmail.com>
2015-11-05 01:20:58 +0800

    尚未加入before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,43 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.build(post_params)
 
+    if @post.save
+      redirect_to group_path(@group), notice: "張貼成功！"
+    else
+      render :new
+    end
   end
 
   def update
-    
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章編輯成功！"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "刪除完畢！"
   end
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
