f2954d910892c39aba84a8a31580deec9f28117b
AlvinChiu <alvinhsian@gmail.com>
2015-11-08 01:33:08 +0800

    尚未完成user 可以加入、退出 group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,20 +6,20 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
     @posts = @group.posts
   end
 
   def new
-    @group = Group.new
+    @group = current_user.groups.new(group_params)
   end
 
   def edit
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
   end
 
   def create
-    @group = Group.create(group_params)
+    @group = current_user.groups.create(group_params)
     if @group.save
       redirect_to groups_path, notice: '新增討論版成功'
     else
@@ -28,7 +28,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
     if @group.update(group_params)
       redirect_to group_path, notice: '修改完成'
     else
@@ -37,7 +37,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: '討論版已經刪除'
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,10 @@ class Group < ActiveRecord::Base
   # 這邊的 :owner 等同於 :user
   # :owner這個欄位可以任意指定名稱，但不管什麼名稱都還是對應到User這個model
   belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  # 驗證 "登入的使用者" 跟 "作者的 id" 是否一致
+  def editable_by?(user)
+    user && user == owner
+  end
+
 end
