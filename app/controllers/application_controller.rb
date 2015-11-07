0e625c3ec54cafb7137e740ba6fdacd6853c436a
AlvinChiu <alvinhsian@gmail.com>
2015-11-07 11:58:11 +0800

    尚未完成只有作者才能有 group 的修改/刪除權限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :configure_permited_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_permitted_parameters, :if => :devise_controller?
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer_.for(:sign_up){
+    devise_parameter_sanitizer.for(:sign_up){
+      |u| u.permit(:name, :email, :password, :password_confirmation)
+    }
+
+    devise_parameter_sanitizer.for(:account_update){
       |u| u.permit(:name, :email, :password, :password_confirmation)
     }
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ActiveRecord::Base
   validates :title, :description, presence: true
   has_many :posts
+
+  # 這邊的 :owner 等同於 :user
+  # :owner這個欄位可以任意指定名稱，但不管什麼名稱都還是對應到User這個model
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 end
